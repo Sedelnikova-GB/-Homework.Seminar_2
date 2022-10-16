c9f7df9121fc07033656b4e525d980385ae7e5b7
Galina <galina_g_sg@mail.ru>
2022-10-16 14:56:43 +0500

    решила задачу 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,36 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+        if (i != mass.Length - 1)
+        {
+            Console.Write(", ");
+
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите масссив чисел: ");
+string str = Console.ReadLine()!;
+int[] array = GetArrayFromString(str);
+PrintArray(array);
+Console.WriteLine();
+
