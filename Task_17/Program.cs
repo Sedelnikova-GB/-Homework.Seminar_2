2d5fecd41465dfc3067fd2df2ad573af976f33f5
Galina <galina_g_sg@mail.ru>
2022-10-16 17:35:56 +0500

    решила задачи 34,36,38

---
+++ Task_17/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+//и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Clear();
+Console.Write("Введите Х: ");
+int x = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите Y: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x > 0)
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    } 
+    else
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+}
+else
+{
+    if (y > 0)
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+    else
+    {
+        Console.WriteLine("Третья четверть");
+    }
+}
+
---
+++ Task_38/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов
+// массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int FindMax(int[] array)
+{
+    int i = 1;
+    int max = array[0];
+    while (i < array.Length)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        i++;
+    }
+    return max;
+}
+
+int FindMin(int[] array)
+{
+    int i = 1;
+    int min = array[0];
+    while (i < array.Length)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        i++;
+    }
+    return min;
+}
+
+Console.Clear();
+Console.Write("Введите масссив чисел: ");
+string str = Console.ReadLine()!;
+int[] array = GetArrayFromString(str);
+int min = FindMin(array);
+int max = FindMax(array);
+Console.WriteLine(max - min);
+
